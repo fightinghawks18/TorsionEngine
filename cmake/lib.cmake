5b1c44c6b76dadeed19f4f6ca724af3260b68bce
fightinghawks18 <lepagetyler@hotmail.com>
2025-08-02 15:08:45 -0500

    - Replaced add_library with create_library to be more faster for my needs in cmake - os library now uses create_library

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -1,8 +1,15 @@
-# Serves the same purpose as add_library, but provides some automation
-# May be more limited than add_library but it fits the needs of the engine
-function(install_library lib lib_name)
+function(create_library lib_name sources)
+    add_library(${lib_name} SHARED
+        ${sources}
+        "${SWIG_GEN}/${lib_name}_wrap.cpp"
+    )
 
-    set_target_properties(${lib}
+    target_include_directories(${lib_name} PRIVATE 
+        "${CMAKE_CURRENT_SOURCE_DIR}/.."
+        "${SWIG_GEN}"
+    )
+
+    set_target_properties(${lib_name}
         PROPERTIES
         PREFIX ""
         OUTPUT_NAME ${lib_name}
